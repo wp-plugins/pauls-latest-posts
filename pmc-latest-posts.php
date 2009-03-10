a4032debd6975bb8dff4e67061bb250f3ecc704b
paulmac <paulmac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-10 02:35:03 +0000

    Updated to version 1.2 - added ability to show latest comments.
    git-svn-id: https://plugins.svn.wordpress.org/pauls-latest-posts/trunk@100878 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pmc-latest-posts.php
+++ pmc-latest-posts.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Pauls Latest Posts
-Plugin URI: http://www.paulmc.org/whatithink/wordpress/plugins/
+Plugin URI: http://www.paulmc.org/whatithink/wordpress/plugins/pauls-latest-posts/
 Description: Plugin to display your latest posts with excerpt in a sidebar widget.
 Author: Paul McCarthy
-Version: 1.1
+Version: 1.2
 Author URI: http://www.paulmc.org/whatithink
 */
 
@@ -29,6 +29,8 @@ function widget_pmcLatestPosts_init() {
 			$pmcNumPosts = $pmcOptions['pmc_num_posts'];
 			$pmcExcerptSize = $pmcOptions['pmc_size'];
 			$pmcPostOffset = $pmcOptions['pmc_post_offset'];
+			$pmcShowComments = $pmcOptions['pmc_show_comments'];
+			$pmcNumComments = $pmcOptions['pmc_num_comments'];
 			
 			//build the parameter list for get_posts
 			$pmcParameters = 'numberposts=' . $pmcNumPosts . '&offset=' . $pmcPostOffset;
@@ -37,8 +39,13 @@ function widget_pmcLatestPosts_init() {
 
 			//start building the list
 			echo $before_widget . $before_title . $pmcTitle . $after_title;
+			
+			//echo the Posts heading
+			echo '<h3 class="pmc-h3">Posts</h3>';
 
 			//start the list to hold the output
+			echo '<ul>';
+			
 			//loop through the posts
 			foreach ($pmcPosts as $pmcSinglePost) {
 		
@@ -53,21 +60,42 @@ function widget_pmcLatestPosts_init() {
 				$pmcTITLE = $pmcSinglePost->post_title;
 						
 				//create the link
-				echo '<h3 class="pmc-h3"><a class="pmc-link" href="' . $pmcHREF . '">' . $pmcTITLE . '</a></h3>';
+				echo '<li><a class="pmc-link" href="' . $pmcHREF . '">' . $pmcTITLE . '</a><br />';
 
 				//check to see if the excerpt size is set to 0
 				//if so the we won't display it.
 				if ($pmcExcerptSize != 0) {
 					//output the excerpt
 					//we use the_content_rss as this allows us to set a size for the excerpt
-					echo '<p class="pmc-excerpt">';
+					echo '<span class="pmc-excerpt">';
 					the_content_rss('', TRUE, '', $pmcExcerptSize);
-					echo '</p>';
+					echo '</span></li>';
 				}
 				
 			} //close foreach
 			
+			//close the list
+			echo '</ul>';
 			
+			//check if the user wants to display the latest comments
+			if ($pmcShowComments) {	
+				//get the comment rss feed URL
+				$commentrss = get_bloginfo('comments_rss2_url');
+				
+				//set the title
+				$pmcTitle = '<h3 class="pmc-h3">Comments</h3>';
+				
+				//we need the in-built WP RSS parser for this to work
+				require_once(ABSPATH . WPINC . '/rss-functions.php');
+				
+				//build the list
+				echo $pmcTitle;
+				echo '<ul>';
+				//get the rss feed
+				get_rss($commentrss, $pmcNumComments);
+				echo '</ul>';
+			}
+		
 			//finish off the widget
 			echo $after_widget;
 		} //close widget_pmcLatestPosts
@@ -83,6 +111,8 @@ function widget_pmcLatestPosts_init() {
 				$newoptions['pmc_num_posts'] = (int) $_POST['pmc_num_posts'];
 				$newoptions['pmc_size'] = (int) $_POST['pmc_size'];
 				$newoptions['pmc_post_offset'] = (int) $_POST['pmc_post_offset'];
+				$newoptions['pmc_show_comments'] = $_POST['pmc_show_comments'];
+				$newoptions['pmc_num_comments'] = $_POST['pmc_num_comments'];
 				} //close if
 			
 			//if there's been a change, do an update
@@ -92,30 +122,43 @@ function widget_pmcLatestPosts_init() {
 			} //close if
 
 			//These are the default settings
-			if ( !$options['pmc_title'] ) $options['pmc_title'] = 'Latest Posts';
+			if ( !$options['pmc_title'] ) $options['pmc_title'] = 'Latest Activity';
 			if ( !$options['pmc_num_posts'] ) $options['pmc_num_posts'] = 5;
 			if ( !$options['pmc_size'] ) $options['pmc_size'] = 25;
 			if ( !$options['pmc_post_offset'] ) $options['pmc_post_offset'] = 1;
+			if ( !$options['pmc_show_comments'] ) $options['pmc_show_comments'] = "";
+			if ( !$options['pmc_num_comments'] ) $options['pmc_num_comments'] = 5;
 			
 			//store the options we got from the database
 			$pmcTitle = $options['pmc_title'];
 			$pmcNumPosts = $options['pmc_num_posts'];
 			$pmcSize = $options['pmc_size'];
 			$pmcPostOffset = $options['pmc_post_offset'];
+			$pmcShowComments = $options['pmc_show_comments'];
+			$pmcNumComments = $options['pmc_num_comments'];
 
 			//strip any html characters from the title
 			$pmcTitle = htmlspecialchars($options['pmc_title'], ENT_QUOTES);
+			
+			//WP stores a check box as Null or "on", in order to display properly, we need to convert "on" to "checked=yes"
+			if ($pmcNumComments) {
+				$pmcChecked = ' checked="yes" ';
+			} else {
+				$pmcChecked = '';
+			}
 			
 		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_title">' . __('Title:') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_title" name="pmc_title" type="text" value="'.$pmcTitle.'" /></label></p>';
 		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_num_posts">' . __('Number of Posts:', 'widgets') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_num_posts" name="pmc_num_posts" type="text" value="'.$pmcNumPosts.'" /></label></p>';
 		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_size">' . __('Excerpt Size:', 'widgets') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_size" name="pmc_size" type="text" value="'.$pmcSize.'" /></label></p>';
 		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_post_offset">' . __('Post Offset:') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_post_offset" name="pmc_post_offset" type="text" value="'.$pmcPostOffset.'" /></label></p>';
+		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_show_comments">' . __('Show Comments:') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_post_offset" name="pmc_show_comments" type="checkbox"'.$pmcShowComments.'" /></label></p>';
+		echo '<p style="margin: 20px auto;"><label style="display: block; width:300px; text-align: centre;" for="pmc_num_comments">' . __('Number of Comments:') . ' <input style="display: block; width: 300px; text-align: left;" id="pmc_post_offset" name="pmc_num_comments" type="text" value="'.$pmcNumComments.'" /></label></p>';
 		echo '<input type="hidden" id="pmc_latest_posts_submit" name="pmc_latest_posts_submit" value="1" />';
 		}
 
 	//register the widget
 	register_sidebar_widget('Pauls Latest Posts', 'widget_pmcLatestPosts');
-	register_widget_control('Pauls Latest Posts', 'pmcLatestPosts_control', 300, 300 );
+	register_widget_control('Pauls Latest Posts', 'pmcLatestPosts_control', 300, 400 );
 
 }
 
--- readme.txt
+++ readme.txt
@@ -2,30 +2,35 @@
 Contributors: paulmac
 Tags: posts, excerpts, lastest, widget, sidebar
 Requires at least: 2.3
-Tested up to: 2.7
-Stable tag: 1.0
+Tested up to: 2.7.1
+Stable tag: 1.2
 
-Display latest post with excerpts in a sidebar widget.
+Display latest posts with excerpts and comments in a sidebar widget.
 
 == Description ==
 
-Pauls Latest Pots displays a list of your latest post in he sidebar with excerpts. Excerpts length can be modified or left out completely. Usefull for sites that display one post on the main page, and want to link to previous posts.
+Pauls Latest Pots displays a list of your latest posts in the sidebar with excerpts. Excerpts length can be modified or left out completely. Useful for sites that display one post on the main page, and want to link to previous posts.
+
+Since version 1.2, Pauls Latest Posts also shows latest comments.
 
 == Installation ==
 
-1. Copy pmc-latest-posts.php to your wp-content/plugins folder.
+1. Extract pauls-latest-posts.zip
+1. Upload pauls-latest-posts.php to your WordPress plugin directory.
 1. Activate the plugin from the 'Plugins' menu in WordPress.
 1. Add the widget to your sidebar from the 'Widgets' menu in WordPress.
 
 == Frequently Asked Questions ==
 
 = What settings can I change? =
-The plugin page allows you to change the following settings: Title, Number of Posts, Excerpt Size and Post Offset.
+The plugin page allows you to change the following settings: Title, Number of Posts, Excerpt Size, Post Offset, Show Comments and Number of Comments.
 
 1. Title: Title of the Sidebar widget.
 1. Number of Posts: Number of posts to show in the Sidebar.
 1. Excerpt Size: Number of words to display in the excerpt for the post.
 1. Post Offset: The number of posts to skip before displaying the list in the Sidebar. Numbering starts from 0. To ignore the latest post, set the offset to 1.
+1. Show Comments: Should the widget also display your latest comments.
+1. Number of Comments: Number of comments to show in the Sidebar.
 
 = What if I don't want to show the excerpt? =
 Change the Excerpt Size to 0 and no excerpt will be inserted.
@@ -33,16 +38,21 @@ Change the Excerpt Size to 0 and no excerpt will be inserted.
 = How do I change the CSS style of the plugin? =
 The following CSS classes are used in the plugin:
 
-* Title: h3.pmc-h3
+* Titles: h3.pmc-h3
 * Link: a.pmc-link
-* Excerpt: p.pmc-excerpt
+* Excerpt: span.pmc-excerpt
 
 Add the class names to your theme's style.css with your custom css.
 
 = I'm using an old version of WordPress, will this plugin work? =
-This plugin uses the Widget API built into WordPress since version 2.3.
-Therefore, this plugin will not work on versions older than 2.3.
-If you are using a version of WordPress prior to 2.3, then I suggest that you upgrade to the latest version.
+This plugin uses the Widget API built into WordPress since version 2.3. Therefore, this plugin will not work on versions older than 2.3. If you are using a version of WordPress prior to 2.3, then I suggest that you upgrade to the latest version.
 
 = Can I re-use your code in my own plugin? =
 If you want to, then yes. This plugin is released under the GPL. You are free to modify it, change it, re-distribute it as you want.
+
+= What other functionality are you thinking of adding? =
+I'm looking into adding a list of latest uploads to the plugin.
+
+== Thanks ==
+
+Thanks goes to Simon who suggested that I add the Latest Comments functionality. If you would like to see anything else added to the list, please do not hesitate to leave a comment on the plugin homepage.
